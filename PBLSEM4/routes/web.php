e1f0b6efa602f658137b18db7fad6a9a74105a5a
mrfauzii <reishifauzi24@gmail.com>
2025-05-08 13:29:36 +0700

    add: new admin menu - user controller route

--- PBLSEM4/routes/web.php
+++ PBLSEM4/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +16,13 @@ use App\Http\Controllers\WelcomeController;
 */
 
 Route::get('/', [WelcomeController::class, 'index']);
+
+// Data User Routes
+Route::prefix('user')->group(function() {
+    Route::get('admin', [UserController::class, 'admin'])->name('user-admin');
+    Route::get('mahasiswa', [UserController::class, 'mahasiswa'])->name('user-mahasiswa');
+    Route::get('dosen', [UserController::class, 'dosen'])->name('user-dosen');
+    Route::get('tendik', [UserController::class, 'tendik'])->name('user-tendik');
+});
+
+
