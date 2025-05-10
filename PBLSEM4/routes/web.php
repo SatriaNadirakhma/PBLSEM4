ad3214888736aceb9155e0bca0646e278b08057b
aqilaazza <aqilanurazza123@gmail.com>
2025-05-10 13:31:52 +0700

    add: MVC (kampus)

--- PBLSEM4/routes/web.php
+++ PBLSEM4/routes/web.php
@@ -3,23 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\KampusController;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
 Route::pattern('id', '[0-9]+');
 
-// Route untuk login dan logout
+// Login & Register
 Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'postlogin']);
 Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
@@ -27,17 +23,23 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->n
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/register', [AuthController::class, 'postRegister']);
 
-Route::middleware(['auth'])->group(function () {
+// Grup rute yang butuh autentikasi
+//Route::middleware(['auth'])->group(function () {
     Route::get('/', [WelcomeController::class, 'index']);
 
+    // Rute kampus
+    Route::prefix('kampus')->group(function () {
+        Route::get('/', [KampusController::class, 'index']);
+    });
+
+    // Rute user
     Route::prefix('user')->group(function () {
         Route::get('/profile', [UserController::class, 'profile']);
         Route::post('/profile/update-photo', [UserController::class, 'updatePhoto'])->name('user.updatePhoto');
 
-        //
         Route::get('/create', [UserController::class, 'create']);
         Route::get('/{id}', [UserController::class, 'show']);
-        Route::get('/{id}/edit', [UserController::class, 'edit']);  
+        Route::get('/{id}/edit', [UserController::class, 'edit']);
         Route::get('/', [UserController::class, 'index']);
         Route::post('/list', [UserController::class, 'list']);
         Route::post('/', [UserController::class, 'store']);
@@ -49,9 +51,9 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/{id}/show_ajax', [UserController::class, 'show'])->name('User.show_ajax');
         Route::put('/{id}', [UserController::class, 'update']);
         Route::delete('/{id}', [UserController::class, 'destroy']);
-        Route::get('import', [UserController::class, 'import']);
-        Route::post('import_ajax', [UserController::class, 'import_ajax']);
-        Route::get('export_excel', [UserController::class, 'export_excel']);
-        Route::get('export_pdf', [UserController::class, 'export_pdf']);
+        Route::get('/import', [UserController::class, 'import']);
+        Route::post('/import_ajax', [UserController::class, 'import_ajax']);
+        Route::get('/export_excel', [UserController::class, 'export_excel']);
+        Route::get('/export_pdf', [UserController::class, 'export_pdf']);
     });
-});
+//});
