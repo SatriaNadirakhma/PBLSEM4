8277b4cc8773779fdf42196a4614cf508d2129b5
SatriaNadiRakhma <rakhmadanisatria@gmail.com>
2025-05-19 12:55:29 +0700

    Enhance: Update action buttons in Jurusan and Kampus

--- PBLSEM4/app/Http/Controllers/KampusController.php
+++ PBLSEM4/app/Http/Controllers/KampusController.php
@@ -43,11 +43,21 @@ class KampusController extends Controller
         return DataTables::of($kampus)
             ->addIndexColumn() // Menambahkan kolom index
             ->addColumn('aksi', function ($k) {
-                return '
-                    <button onclick="modalAction(\'' . url('/kampus/' . $k->kampus_id . '/show_ajax') . '\')" class="btn btn-info btn-sm me-1">Detail</button>
-                    <button onclick="modalAction(\'' . url('/kampus/' . $k->kampus_id . '/edit_ajax') . '\')" class="btn btn-warning btn-sm me-1">Edit</button>
-                    <button onclick="modalAction(\'' . url('/kampus/' . $k->kampus_id . '/delete_ajax') . '\')" class="btn btn-danger btn-sm">Hapus</button>
-                ';
+                $btn  = '<button onclick="modalAction(\'' . url('/kampus/' . $k->kampus_id . '/show_ajax') . '\')" 
+                            class="btn btn-info btn-sm rounded-pill shadow-sm me-1 px-3 py-1" style="font-size: 0.85rem;">
+                            <i class="fa fa-eye me-1"></i> Detail
+                        </button>';
+
+                $btn .= '<button onclick="modalAction(\'' . url('/kampus/' . $k->kampus_id . '/edit_ajax') . '\')" 
+                            class="btn btn-warning btn-sm rounded-pill shadow-sm me-1 px-3 py-1" style="font-size: 0.85rem;">
+                            <i class="fa fa-edit me-1"></i> Edit
+                        </button>';
+
+                $btn .= '<button onclick="modalAction(\'' . url('/kampus/' . $k->kampus_id . '/delete_ajax') . '\')" 
+                            class="btn btn-danger btn-sm rounded-pill shadow-sm px-3 py-1" style="font-size: 0.85rem;">
+                            <i class="fa fa-trash me-1"></i> Hapus
+                        </button>';
+                return $btn;
             })
             ->rawColumns(['aksi']) // Memberitahu bahwa kolom aksi adalah HTML
             ->make(true);
