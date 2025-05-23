b713df0f3151b64af2b23781e44503d662315579
aqilaazza <aqilanurazza123@gmail.com>
2025-05-23 11:00:50 +0700

    Update view dosen menu

--- PBLSEM4/resources/views/biodata/dosen/index.blade.php
+++ PBLSEM4/resources/views/biodata/dosen/index.blade.php
@@ -69,9 +69,6 @@
                         <th>NIDN</th>
                         <th>NIK</th>
                         <th>Nama</th>
-                        <th>No. Telp</th>
-                        <th>Alamat Asal</th>
-                        <th>Alamat Sekarang</th>
                         <th>Jenis Kelamin</th>
                         <th>Jurusan</th>
                         <th>Aksi</th>
@@ -111,9 +108,6 @@
                 { data: "nidn", className: "text-nowrap" },
                 { data: "nik", className: "text-nowrap" },
                 { data: "dosen_nama", className: "text-nowrap" },
-                { data: "no_telp", className: "text-nowrap" },
-                { data: "alamat_asal", className: "text-nowrap" },
-                { data: "alamat_sekarang", className: "text-nowrap" },
                 { data: "jenis_kelamin", className: "text-center" },
                 { data: "jurusan_id", className: "text-center" },
                 { data: "aksi", className: "text-center text-nowrap", orderable: false, searchable: false }
