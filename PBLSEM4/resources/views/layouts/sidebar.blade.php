71066a62a2ab15ad62ad75c9852ee50e5b1f209c
SatriaNadiRakhma <rakhmadanisatria@gmail.com>
2025-05-26 09:58:46 +0700

    refactor: improve sidebar layout and spacing

--- PBLSEM4/resources/views/layouts/sidebar.blade.php
+++ PBLSEM4/resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="sidebar">
     <!-- SidebarSearch Form -->
-    <div class="form-inline mt-2">
+    <!-- <div class="form-inline mt-2">
         <div class="input-group" data-widget="sidebar-search">
             <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
             <div class="input-group-append">
@@ -9,10 +9,10 @@
                 </button>
             </div>
         </div>
-    </div>
+    </div> -->
 
     <!-- Sidebar Menu -->
-    <nav class="mt-2">
+    <nav class="mt-3">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="true">
             <li class="nav-item">
                 <a href="{{ url('/dashboard') }}" class="nav-link {{ ($activeMenu == 'dashboard') ? 'active' : '' }}">
@@ -23,7 +23,7 @@
             <!--DATA USER -->
             @auth
             @if(Auth::user()->role == 'admin')
-            <li class="nav-item">
+            <li class="nav-item mt-1">
                 <a href="{{ route('user') }}" class="nav-link {{ in_array($activeMenu, ['user']) ? 'active' : '' }}">
                     <i class="nav-icon fas fa-layer-group"></i>
                     <p>DATA USER</p>
@@ -36,7 +36,7 @@
             <!-- Dropdown BIODATA -->
             @auth
             @if(Auth::user()->role == 'admin')
-            <li class="nav-item has-treeview {{ in_array($activeMenu, ['peserta-mahasiswa', 'peserta-dosen', 'peserta-tendik', 'admin']) ? 'menu-open' : '' }}">
+            <li class="nav-item mt-1 has-treeview {{ in_array($activeMenu, ['peserta-mahasiswa', 'peserta-dosen', 'peserta-tendik', 'admin']) ? 'menu-open' : '' }}">
                 <a href="#" class="nav-link {{ in_array($activeMenu, ['peserta-mahasiswa', 'peserta-dosen', 'peserta-tendik', 'admin']) ? 'active' : '' }}">
                     <i class="nav-icon fas fa-users"></i>
                     <p>
@@ -46,7 +46,7 @@
                 </a>
                 <ul class="nav nav-treeview">
                     @if(Auth::user()->role == 'admin')
-                    <li class="nav-item">
+                    <li class="nav-item ml-2">
                         <a href="{{ url('/biodata/mahasiswa') }}" class="nav-link {{ ($activeMenu == 'peserta-mahasiswa') ? 'active' : '' }}">
                             <i class="far fa-user nav-icon"></i>
                             <p>Peserta Mahasiswa</p>
@@ -54,7 +54,7 @@
                     </li>
                     @endif
                     @if(Auth::user()->role == 'admin')
-                    <li class="nav-item">
+                    <li class="nav-item ml-2">
                         <a href="{{ url('/biodata/dosen') }}" class="nav-link {{ ($activeMenu == 'peserta-dosen') ? 'active' : '' }}">
                             <i class="far fa-user nav-icon"></i>
                             <p>Peserta Dosen</p>
@@ -62,7 +62,7 @@
                     </li>
                     @endif
                     @if(Auth::user()->role == 'admin')
-                    <li class="nav-item">
+                    <li class="nav-item ml-2">
                         <a href="{{ route('biodata.tendik.index') }}" class="nav-link {{ ($activeMenu == 'peserta-tendik') ? 'active' : '' }}">
                             <i class="far fa-user nav-icon"></i>
                             <p>Peserta Tendik</p>
@@ -70,7 +70,7 @@
                     </li>
                     @endif
                     @if(Auth::user()->role == 'admin')
-                    <li class="nav-item">
+                    <li class="nav-item ml-2">
                         <a href="{{ url('/admin') }}" class="nav-link {{ ($activeMenu == 'admin') ? 'active' : '' }}">
                             <i class="far fa-user nav-icon"></i>
                             <p>Admin</p>
@@ -85,7 +85,7 @@
             <!-- Dropdown DAFTAR KAMPUS -->
             @auth
             @if(Auth::user()->role == 'admin')
-            <li class="nav-item has-treeview {{ in_array($activeMenu, ['kampus', 'jurusan', 'prodi']) ? 'menu-open' : '' }}">
+            <li class="nav-item mt-1 has-treeview {{ in_array($activeMenu, ['kampus', 'jurusan', 'prodi']) ? 'menu-open' : '' }}">
                 <a href="#" class="nav-link {{ in_array($activeMenu, ['kampus', 'jurusan', 'prodi']) ? 'active' : '' }}">
                     <i class="nav-icon fas fa-home"></i>
                     <p>
@@ -94,19 +94,19 @@
                     </p>
                 </a>
                 <ul class="nav nav-treeview">
-                    <li class="nav-item">
+                    <li class="nav-item ml-2">
                         <a href="{{ url('/kampus') }}" class="nav-link {{ ($activeMenu == 'kampus') ? 'active' : '' }}">
                             <i class="fa fa-list nav-icon"></i>
                             <p>Daftar Kampus</p>
                         </a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item ml-2">
                         <a href="{{ url('/jurusan') }}" class="nav-link {{ ($activeMenu == 'jurusan') ? 'active' : '' }}">
                             <i class="fa fa-list nav-icon"></i>
                             <p>Daftar Jurusan</p>
                         </a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item ml-2">
                         <a href="{{ url('/prodi') }}" class="nav-link {{ ($activeMenu == 'prodi') ? 'active' : '' }}">
                             <i class="fa fa-list nav-icon"></i>
                             <p>Daftar Program Studi</p>
@@ -120,7 +120,7 @@
             <!-- Dropdown PENDAFTARAN -->
             @auth
             @if(Auth::user()->role == 'admin')
-            <li class="nav-item has-treeview {{ in_array($activeMenu, ['verifikasi', 'edit-formulir']) ? 'menu-open' : '' }}">
+            <li class="nav-item mt-1 has-treeview {{ in_array($activeMenu, ['verifikasi', 'edit-formulir']) ? 'menu-open' : '' }}">
                 <a href="#" class="nav-link {{ in_array($activeMenu, ['verifikasi', 'edit-formulir']) ? 'active' : '' }}">
                     <i class="nav-icon fas fa-clipboard-list"></i>
                     <p>
@@ -130,7 +130,7 @@
                 </a>
                 <ul class="nav nav-treeview">
                     @if(Auth::user()->role == 'admin')
-                    <li class="nav-item">
+                    <li class="nav-item ml-2">
                         <a href="{{ url('/verifikasi') }}" class="nav-link {{ ($activeMenu == 'verifikasi') ? 'active' : '' }}">
                             <i class="fa fa-list nav-icon"></i>
                             <p>Verifikasi Pendaftaran</p>
@@ -138,7 +138,7 @@
                     </li>
                     @endif
                     @if(Auth::user()->role == 'admin')
-                    <li class="nav-item">
+                    <li class="nav-item ml-2">
                         <a href="{{ url('riwayat') }}" class="nav-link {{ ($activeMenu == 'riwayat-pendaftaran') ? 'active' : '' }}">
                             <i class="fa fa-list nav-icon"></i>
                             <p>Riwayat Pendaftaran</p>
@@ -146,7 +146,7 @@
                     </li>
                     @endif
                     @if(in_array(Auth::user()->role, ['mahasiswa', 'dosen', 'tendik']))
-                    <li class="nav-item">
+                    <li class="nav-item ml-2">
                         <a href="{{ url('/pendaftaran/edit') }}" class="nav-link {{ ($activeMenu == 'edit-formulir') ? 'active' : '' }}">
                             <i class="fa fa-list nav-icon"></i>
                             <p>Formulir</p>
@@ -160,7 +160,7 @@
 
             @auth
             @if(Auth::user()->role == 'admin')
-            <li class="nav-item has-treeview {{ in_array($activeMenu, ['info-jadwal', 'info-pengumuman', 'info-zoom']) ? 'menu-open' : '' }}">
+            <li class="nav-item mt-1 has-treeview {{ in_array($activeMenu, ['info-jadwal', 'info-pengumuman', 'info-zoom']) ? 'menu-open' : '' }}">
                 <a href="#" class="nav-link {{ in_array($activeMenu, ['info-jadwal', 'info-pengumuman', 'info-zoom']) ? 'active' : '' }}">
                     <i class="nav-icon fas fa-info-circle"></i>
                     <p>
@@ -169,19 +169,19 @@
                     </p>
                 </a>
                 <ul class="nav nav-treeview">
-                    <li class="nav-item">
+                    <li class="nav-item ml-2">
                         <a href="{{ url('/informasi/jadwal') }}" class="nav-link {{ ($activeMenu == 'info-jadwal') ? 'active' : '' }}">
                             <i class="far fa-circle nav-icon"></i>
                             <p>Jadwal</p>
                         </a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item ml-2">
                         <a href="{{ url('/informasi/pengumuman') }}" class="nav-link {{ ($activeMenu == 'info-pengumuman') ? 'active' : '' }}">
                             <i class="far fa-circle nav-icon"></i>
                             <p>Pengumuman</p>
                         </a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item ml-2">
                         <a href="{{ url('/informasi/zoom') }}" class="nav-link {{ ($activeMenu == 'info-zoom') ? 'active' : '' }}">
                             <i class="far fa-circle nav-icon"></i>
                             <p>Hasil Ujian</p>
